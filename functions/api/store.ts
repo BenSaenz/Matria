455bad337a9e8f073f96a34d2ecd33dc7fcd6dc2
BenSaenz <punicornio1804@gmail.com>
2026-04-07 22:35:42 -0500

    Update store.ts

--- functions/api/store.ts
+++ functions/api/store.ts
@@ -1,8 +1,8 @@
 import type { Env } from '../_shared/utils';
 
 export const onRequestGet: PagesFunction<Env> = async (context) => {
-const products = await context.env.DB
-  .prepare('SELECT * FROM products LIMIT 5')
+const coupons = await context.env.DB
+  .prepare('SELECT * FROM coupons LIMIT 5')
   .all();
 
   return new Response(JSON.stringify(collections), {
