b6f40e7201899a49cd54a9682225ac20bdcd9d9d
BenSaenz <punicornio1804@gmail.com>
2026-04-07 22:21:12 -0500

    Update store.ts

--- functions/api/store.ts
+++ functions/api/store.ts
@@ -1,18 +1,10 @@
-import type { Env, Section } from './utils';
-import { safeParseJson, touchRevision, readRevision } from './utils';
-
-export async function getStorePayload(env: Env) {
-  const [collectionsResult, productsResult, couponsResult, revision] = await Promise.all([
-    env.DB.prepare(`SELECT * FROM collections ORDER BY sort_order ASC, name ASC`).all<CollectionRow>(),
-    env.DB.prepare(`SELECT * FROM products ORDER BY sort_order ASC, name ASC`).all<ProductRow>(),
-    env.DB.prepare(`SELECT * FROM coupons ORDER BY code ASC`).all<CouponRow>(),
-    readRevision(env)
-  ]);
-
-  return {
-    collections: (collectionsResult.results || []).map(mapCollectionRow),
-    products: (productsResult.results || []).map(mapProductRow),
-    coupons: (couponsResult.results || []).map(mapCouponRow),
-    revision
-  };
-}
+export const onRequestGet: PagesFunction = async () => {
+  return new Response(
+    JSON.stringify({ ok: true, route: "/api/store" }),
+    {
+      headers: {
+        "content-type": "application/json; charset=utf-8"
+      }
+    }
+  );
+};
