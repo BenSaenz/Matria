3a5743cdf9d397f0b2bb3f63fb8766a1234e689e
BenSaenz <punicornio1804@gmail.com>
2026-04-07 22:25:36 -0500

    Update store.ts

--- functions/api/store.ts
+++ functions/api/store.ts
@@ -1,10 +1,10 @@
-export const onRequestGet: PagesFunction = async () => {
-  return new Response(
-    JSON.stringify({ ok: true, route: "/api/store" }),
-    {
-      headers: {
-        "content-type": "application/json; charset=utf-8"
-      }
+import type { Env } from '../_shared/utils';
+
+export const onRequestGet: PagesFunction<Env> = async (context) => {
+  const result = await context.env.DB.prepare('SELECT 1 as ok').first();
+  return new Response(JSON.stringify(result), {
+    headers: {
+      'content-type': 'application/json; charset=utf-8'
     }
-  );
+  });
 };
